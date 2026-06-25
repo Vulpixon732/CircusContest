022dbb1cfcab8f342ba882659538537187ed72fd
Vulpixon732 <kosenok732@gmail.com>
2026-06-25 17:41:01 +0200

    Duck Spawner

--- Assets/Scripts/S_DuckSpawn.cs
+++ Assets/Scripts/S_DuckSpawn.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class S_DuckSpawn : MonoBehaviour
 {
     public GameObject duckPrefab;
+
+    [Header("Random Added Rigidbody Force on Instantiate\n\nx - Min Spawn Force Value\ny - Max Spawn Force Value")]
+    public Vector2 forceX = new Vector2(0f, 0.1f);
+    public Vector2 forceY = new Vector2(0f, 0.1f);
+    public Vector2 forceZ = new Vector2(0f, 0.1f);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +26,8 @@ public class S_DuckSpawn : MonoBehaviour
     {
         if(duckPrefab != null)
         {
-            Instantiate(duckPrefab, this.transform.position, Quaternion.identity);
+            GameObject newDuck = Instantiate(duckPrefab, this.transform.position, Quaternion.identity);
+            newDuck.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(forceX.x, forceX.y), Random.Range(forceY.x, forceY.y), Random.Range(forceZ.x, forceZ.y)), ForceMode.Impulse);
         }
     }
 }
